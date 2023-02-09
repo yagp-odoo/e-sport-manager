c0448da0c133f9c72c6e42e19305639ad4e271a0
Shubham Thanki <shut@odoo.com>
2023-02-09 19:06:52 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Tournaments",
+    'sequence': -99,
+    'depends': ['base'],
+    'application': True,
+    'license': 'LGPL-3',
+    # 'data': ['security/ir.model.access.csv',
+    #          'views/estate_property_views.xml',
+    #          'views/estate_menus.xml']
+}
